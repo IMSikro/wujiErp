7dead22ef0ca204487ada59a927c9e24f3c07917
SikroYang <yxx95lz@163.com>
2023-06-17 11:36:38 +0800

    修改Program以适配.net7.0

--- wujiErp.Web/Program.cs
+++ wujiErp.Web/Program.cs
@@ -1,5 +1,18 @@
-var builder = WebApplication.CreateBuilder(args);
-builder.Inject();
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args).Inject();
+
+builder.Services.AddControllers().AddInject();
 
 var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseInject();
+
+app.MapControllers();
+
 app.Run();
